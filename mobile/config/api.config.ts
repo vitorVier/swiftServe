e8cae1a79efbca96b1fee6a78518378b42aaf15e
vitorVier <vierrvitor@gmail.com>
2026-03-26 14:29:24 -0300

    change base url

--- mobile/config/api.config.ts
+++ mobile/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    // BASE_URL: "http://192.168.1.31", // IP da máquina ou URL do projeto hospedado (backend) - celular
-    BASE_URL: "http://192.168.1.31:3333", // Usar somente no emulador
+    BASE_URL: "https://swiftserve-production.up.railway.app", // IP da máquina ou URL do projeto hospedado (backend)
+    // BASE_URL: "http://192.168.1.31:3333", // Usar somente no emulador
     TIMEOUT: 12000 // 12 segundos
 }
