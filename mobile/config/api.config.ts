49707c1a63f7d72413c1dd9e76d37d9897403fec
vitorVier <vierrvitor@gmail.com>
2026-03-26 15:15:16 -0300

    migrate to render

--- mobile/config/api.config.ts
+++ mobile/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BASE_URL: "https://swiftserve-production.up.railway.app", // IP da máquina ou URL do projeto hospedado (backend)
+    BASE_URL: "https://swiftserve-glto.onrender.com", // IP da máquina ou URL do projeto hospedado (backend)
     // BASE_URL: "http://192.168.1.31:3333", // Usar somente no emulador
     TIMEOUT: 12000 // 12 segundos
 }
